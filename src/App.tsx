b7dd956def30ea2e267cd6aa1ec9e0ba3d3702ac
VKTRN <vktr.ndns@gmail.com>
2022-09-04 11:16:29 +0200

    _

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
-import ReactPDF   from '@react-pdf/renderer'
-import {Left}     from './components/Left'
-import {Right}    from './components/Right'
 import {Resume}    from './components/Resume'
-import {MyDocument} from './components/Document'
 import                 './scss/style.scss'
 
-import {renderToString} from 'react-dom/server';
-
-
 export const App = () => {
-
-  const printPDF = () => {
-    const htmlString = renderToString(<Resume/>)
-
-    console.log(htmlString)
-  }
 
   return (
     <div className = 'app'>
       <Resume/>
-      {/* <button className = 'print-pdf' onClick={printPDF}>print</button> */}
     </div>
   )
 }
--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,11 +1,15 @@
 import {Left}     from './Left'
 import {Right}    from './Right'
+import {MdPictureAsPdf} from 'react-icons/md'
 
 export const Resume = () => {
   return (
     <div className='resume'>
       <Left />
       <Right />
+      <a href = './Resume.pdf' target="_blank">
+        <MdPictureAsPdf className = 'open-pdf'  />
+      </a>
   </div>
   )
 }
